36b5b0a7a49797a7adf323fdf036e69c5d879ce5
JustoBarbosa <jbarbosamoreira@udesa.edu.ar>
2026-04-04 17:25:45 -0300

    complete metricas.py

--- src/metricas.py
+++ src/metricas.py
@@ -1,8 +1,39 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr  4 16:24:54 2026
+def calcular_tiempo_total(datos):
+    
+    total = 0
+    
+    for participantes in datos:
+        for tiempo in participantes["tiempo_uso"]:
+            total = total + tiempo
+    
+    return total
 
-@author: justo
-"""
+def calcular_promedio_uso(datos):
+    
+    total_uso = 0
+    total_registros = 0
+    
+    for participante in datos:
+        for cantidad in participante["cantidad_uso"]:
+            total_uso += cantidad
+            total_registros += 1
+            
+    if total_registros == 0:
+        return 0.0
+    
+    return total_uso / total_registros
 
-print("prueba")
+def calcular_uso_por_app(datos):
+    
+    uso_por_app = {}
+    
+    for participante in datos:
+        for i in range(len(participante["app"])):
+            app = participante["app"][i]
+            tiempo = participante["cantidad_uso"][i]
+            if app not in uso_por_app:
+                uso_por_app[app] = 0
+            uso_por_app[app] += tiempo
+    
+    return uso_por_app
+            
