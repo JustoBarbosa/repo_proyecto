9d3286ccd593aa66e281a95149086a4dfc1944dc
solcollins <mcollins@udesa.edu.ar>
2026-04-05 12:48:59 -0300

    Update procesamiento_datos.py
    hice la funcion filtrar_por_participante

--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr  4 16:24:53 2026
+def filtrar_por_participante (datos, id_participante): 
+   
+    participante = []
+    for participantes in datos: 
+        if participantes["id_participante"] == id_participante: 
+            participante.append(participantes)
+            
+    return participante 
 
-@author: justo
-"""
-
-print("prueba")
