0221de4f0413a5d0d732dc5bcd46b4035e43d9ac
solcollins <mcollins@udesa.edu.ar>
2026-04-05 20:15:44 -0300

    Update procesamiento_datos.py

--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -1,4 +1,18 @@
 def filtrar_por_participante (datos, id_participante): 
+  
+    """
+    Recorre una lista con los datos de todos los participantes, donde cada participante es un diccionario. 
+    Si el id del participante coincide con el id del parametro de la funcion, agrega a dicho participante a una lista y la devuelve.  
+    
+    Parametros:
+        
+        datos(lista): es una lista de diccionarios, donde cada diccionario son los datos de cada participante.
+        id_participante(int): es el numero de identificacion del participante del que se quieren saber los datos
+        
+    Retorna: 
+        
+        participante(lista): una lista con los datos del participante seleccionado 
+    """
    
     participante = []
     for participantes in datos: 
