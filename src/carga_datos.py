227ee3ef448f66dd355390617db44070f072e558
vazpeitia <vazpeitia@udesa.edu.ar>
2026-04-05 21:08:56 -0300

    Update carga_datos.py
    Hice el docstring de las funciones

--- src/carga_datos.py
+++ src/carga_datos.py
@@ -1,5 +1,18 @@
 def parsear_linea(linea):
+
+    '''
+    Esta funcion se va a encargar de recibir datos extraidos de un archivo y los va a separar\
+    en los datos de cada participante especificando por cada linea un id, una fecha, una app de\
+    uso, su cantidad y tiempo.
+
+    Parameters: 
+        linea: str. Recibe una linea proveniente de un archivo con datos de los usos de cada usuario
     
+    Return:
+        id_participante, fecha, app, cantidad_uso, tiempo_uso: lista.
+        Es una lista donde se dividieron los datos de cada participante en ese orden puntual para facilitar\
+        la posterior medicion de los datos.
+    '''    
     linea = linea.strip()
     campos = linea.split(",")
     
@@ -13,6 +26,19 @@ def parsear_linea(linea):
 
 def cargar_datos(ruta):
     
+    '''
+    Esta funcion recibe un archivo, lo va a leer y va a utilizar la funcion parsear_linea para separar\
+    los datos en lo que corresponde a cada participante. En base a esta lista va a separar los datos y \
+    determinar si ya fueron agregados o no al diccionario de participantes. Si no fueron agregados clasifica\
+    cada id, fecha, app, cantidad de uso y tiempo de uso en cada clave, siendo los datos el valor de las mismas. 
+    Se devuelve una lista de los valores de ese diccionario.
+    
+    Parameters:
+        ruta: archivo. Se ingresan los datos sobre los cuales se van a calcular las mediciones en posteriores funciones.
+        
+    Return:
+        lista. Se devuelven los valores del diccionario creado a partir de la extraccion de datos.
+    '''
     participantes = {}
     
     with open(ruta, "r") as archivo:
