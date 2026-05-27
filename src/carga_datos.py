ed4b17d0582bb4fd8131762afb325eb2a5989c1a
solcollins <mcollins@udesa.edu.ar>
2026-05-27 09:22:19 -0300

    Update carga_datos.py
    modifique el docstring de carga_datos

--- src/carga_datos.py
+++ src/carga_datos.py
@@ -5,15 +5,10 @@ import os
 def cargar_datos(ruta):
     
     '''
-    Esta funcion recibe un archivo, lo va a leer y va a utilizar la funcion parsear_linea para separar\
-    los datos en lo que corresponde a cada participante. En base a esta lista va a separar los datos y \
-    determinar si ya fueron agregados o no al diccionario de participantes. Si no fueron agregados clasifica\
-    cada id, fecha, app, cantidad de uso y tiempo de uso en cada clave, siendo los datos el valor de las mismas. 
-    Se devuelve una lista de los valores de ese diccionario. (pre)
-    (post): Carga el arciho csv en un DataFrame de pandas. asigna los nombres de columnas correspondientes
-    
+    Carga un archivo CSV y lo retorna como un DataFrame de pandas
+   
     Parameters:
-        ruta (str): archivo. Se ingresan los datos sobre los cuales se van a calcular las mediciones en posteriores funciones.
+        ruta (str): ruta al archivo CSV con los datos sobre los cuales se van a calcular las mediciones en posteriores funciones.
         
     Return:
         DataFrame: DataFrame con columnas id_participante, fecha, app, cantidad_uso, tiempo_uso
